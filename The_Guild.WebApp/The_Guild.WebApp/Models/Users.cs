ad9b985c80c27c7dfc8d770f4739943216b93dab
esqueda6021 <o_e10@outlook.com>
2019-03-21 21:38:40 -0500

    AddModels

---
+++ The_Guild.WebApp/The_Guild.WebApp/Models/Users.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Guild.WebApp.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public int LoginInfoId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+
+        [Range(0,900000)]
+        public decimal? Salary { get; set; }
+
+        
+        [Range(0, 900000)]
+        public int? Strength { get; set; }
+                
+        [Range(0, 900000)]
+        public int? Dex { get; set; }
+                
+        [Range(0, 900000)]
+        public int? Wisdom { get; set; }
+                
+        [Range(0, 900000)]
+        public int? Intelligence { get; set; }
+                
+        [Range(0, 900000)]
+        public int? Charisma { get; set; }
+                
+        [Range(0, 900000)]
+        public int? Constitution { get; set; }
+             
+        [Range(0, 900000)]
+        public int? RankId { get; set; }
+
+        public IEnumerable<AdventureParty> AdventureParty { get; set; }
+        public IEnumerable<RequestingParty> RequestingParty { get; set; }
+    }
+}
