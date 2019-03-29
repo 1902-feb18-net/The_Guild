e64f2cb9d46ed9f24d3b32ed683c2988a0fc7391
Lee <ljmars2351@gmail.com>
2019-03-29 15:26:22 -0500

    register user adds to both dbs

--- The_Guild.WebApp/The_Guild.WebApp/ApiModels/ApiRegister.cs
+++ The_Guild.WebApp/The_Guild.WebApp/ApiModels/ApiRegister.cs
@@ -9,6 +9,17 @@ namespace The_Guild.WebApp.ApiModels
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal? Salary { get; set; }
+        public int? Strength { get; set; }
+        public int? Dex { get; set; }
+        public int? Wisdom { get; set; }
+        public int? Intelligence { get; set; }
+        public int? Charisma { get; set; }
+        public int? Constitution { get; set; }
+        public int? RankId { get; set; }
 
     }
 }
