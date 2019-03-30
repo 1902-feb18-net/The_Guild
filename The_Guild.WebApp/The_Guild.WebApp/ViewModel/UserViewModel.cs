0de2e96d85d298a858f4aa1d3b51ac521b77ad0a
Matthew Valencia <MatthewLuisValencia@gmail.com>
2019-03-30 12:06:30 -0500

    Updated UserViewModel and added tests for it.

--- The_Guild.WebApp/The_Guild.WebApp/ViewModel/UserViewModel.cs
+++ The_Guild.WebApp/The_Guild.WebApp/ViewModel/UserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ardalis.GuardClauses;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,38 +11,162 @@ namespace The_Guild.WebApp.ViewModel
 {
     public class UserViewModel
     {
+        private string _first,
+                      _last;
+        private decimal? _sal;
+        private int? _str,
+                     _dex,
+                     _wis,
+                     _int,
+                     _cha,
+                     _con;
+
         public int Id { get; set; }
 
         [Required]
         [Display(Name = "First Name")]
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get => _first;
+            set
+            {
+                Guard.Against.NullOrWhiteSpace(value, nameof(value));
+                _first = value;
+            }
+        }
 
         [Required]
         [Display(Name = "Last Name")]
-        public string LastName { get; set; }
+        public string LastName
+        {
+            get => _last;
+            set
+            {
+                Guard.Against.NullOrWhiteSpace(value, nameof(value));
+                _last = value;
+            }
+        }
 
 
         [Range(0, 900000)]
-        public decimal? Salary { get; set; }
+        public decimal? Salary
+        {
+            get => _sal;
+            set
+            {
+                if (CheckConstraints.NonNegativeDecimal(value))
+                {
+                    _sal = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
 
         [Range(0, 40)]
-        public int? Strength { get; set; }
+        public int? Strength
+        {
+            get => _str;
+            set
+            {
+                if (CheckConstraints.ValidInt(value))
+                {
+                    _str = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
         [Range(0, 40)]
-        public int? Dex { get; set; }
+        public int? Dex
+        {
+            get => _dex;
+            set
+            {
+                if (CheckConstraints.ValidInt(value))
+                {
+                    _dex = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
         [Range(0, 40)]
-        public int? Wisdom { get; set; }
+        public int? Wisdom
+        {
+            get => _wis;
+            set
+            {
+                if (CheckConstraints.ValidInt(value))
+                {
+                    _wis = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
         [Range(0, 40)]
-        public int? Intelligence { get; set; }
+        public int? Intelligence
+        {
+            get => _int;
+            set
+            {
+                if (CheckConstraints.ValidInt(value))
+                {
+                    _int = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
         [Range(0, 40)]
-        public int? Charisma { get; set; }
+        public int? Charisma
+        {
+            get => _cha;
+            set
+            {
+                if (CheckConstraints.ValidInt(value))
+                {
+                    _cha = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
         [Range(0, 40)]
-        public int? Constitution { get; set; }
+        public int? Constitution
+        {
+            get => _con;
+            set
+            {
+                if (CheckConstraints.ValidInt(value))
+                {
+                    _con = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
         [Range(0, 900000)]
         [Display(Name = "Rank")]
