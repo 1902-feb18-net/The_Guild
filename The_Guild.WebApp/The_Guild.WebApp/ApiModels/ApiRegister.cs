56ea5a011c5bfd2cf9cac626055999f49709d100
Lee <ljmars2351@gmail.com>
2019-03-31 10:11:16 -0500

    adding signed in role checks

--- The_Guild.WebApp/The_Guild.WebApp/ApiModels/ApiRegister.cs
+++ The_Guild.WebApp/The_Guild.WebApp/ApiModels/ApiRegister.cs
@@ -9,8 +9,7 @@ namespace The_Guild.WebApp.ApiModels
     {
         public string Username { get; set; }
         public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        
         
 
     }
